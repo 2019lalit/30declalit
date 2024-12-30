e34e0ce2e99f87935df93f7ad0bd4c0b48e8cfd5
lalit singh <lalitsingh@gmail.com>
2024-12-30 19:25:05 +0530

    c program

--- lalit4.c
+++ lalit4.c
@@ -1,1 +1,16 @@
 write a input two number a and b which one is maximum ?
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    printf("enter the first number:\n");
+    scanf("%d",&a);
+    printf("enter the second number:\n");
+    scanf("%d",&b);
+    if(a>b){
+        printf("a is maximun:\n");
+    }else{
+        printf("b is maximun:\n");
+    }
+    return 0;
+}
--- lalit5.c
+++ lalit5.c
@@ -1,5 +1,24 @@
-write a c program input  character 'a'
-		two number input 
-		addition 
+//write a c program input  character 'a'
+//		two number input 
+//		addition 
 
-	sorry input is not matched 
+//	sorry input is not matched
+//
+#include<stdio.h>
+int main(){
+    char a;
+    printf("enter the character:\n");
+    scanf("%c",&a);
+    if(a=='a'){
+        int b,c,res;
+        printf("first number:\n");
+        scanf("%d",&b);
+         printf("second number number:\n");
+         scanf("%d",&c);
+         res=b+c;
+          printf("the sum of b and c is %d:",res);
+    }else{
+        printf(" sorry input is not matched:\n");
+    }
+    return 0;
+}
